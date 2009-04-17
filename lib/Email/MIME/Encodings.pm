2586720d6ebe60645e52646ae36ddec60260c860
Ricardo SIGNES <rjbs@cpan.org>
2009-04-17 03:49:45 +0000

    Import of RJBS/Email-MIME-Encodings-1.313 from CPAN.
    gitpan-cpan-distribution: Email-MIME-Encodings
    gitpan-cpan-version:      1.313
    gitpan-cpan-path:         RJBS/Email-MIME-Encodings-1.313.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/MIME/Encodings.pm
+++ lib/Email/MIME/Encodings.pm
@@ -3,7 +3,7 @@ use strict;
 no strict 'refs';
 use warnings;
 
-$Email::MIME::Encodings::VERSION = "1.312";
+$Email::MIME::Encodings::VERSION = "1.313";
 
 use MIME::Base64;
 use MIME::QuotedPrint;
@@ -18,12 +18,38 @@ sub codec {
     my ($which, $how, $what) = @_;
     $how = lc $how;
     $how = "qp" if $how eq "quotedprint" or $how eq "quoted-printable";
-    my $sub = $which."_".$how;
-    if (not defined &$sub) {
+    my $sub = __PACKAGE__->can("$which\_$how");
+
+    unless ($sub) {
         require Carp;
         Carp::croak("Don't know how to $which $how");
     }
-    $sub->($what);
+
+    # RFC2822 requires all email lines to end in CRLF. The Quoted-Printable
+    # RFC requires CRLF to not be encoded, when representing newlins.  We will
+    # assume, in this code, that QP is being used for plain text and not binary
+    # data.  This may, someday, be wrong -- but if you are using QP to encode
+    # binary data, you are already doing something bizarre.
+    #
+    # The only way to achieve this with MIME::QuotedPrint is to replace all
+    # CRLFs with just LF and then let MIME::QuotedPrint replace all LFs with
+    # CRLF. Otherwise MIME::QuotedPrint (by default) encodes CR as =0D, which
+    # is against RFCs and breaks MUAs (such as Thunderbird).
+    #
+    # We don't modify data before Base64 encoding it because that is usually
+    # binary data and modifying it at all is a bad idea. We do however specify
+    # that the encoder should end lines with CRLF (since that's the email
+    # standard).
+    # -- rjbs and mkanat, 2009-04-16
+    my $eol = "\x0d\x0a";
+    if ($which eq 'encode') {
+        $what =~ s/$eol/\x0a/sg if $how eq 'qp';
+        return $sub->($what, $eol);
+    } else {
+        my $txt = $sub->($what);
+        $txt =~ s/\x0a/$eol/sg if $how eq 'qp';
+        return $txt;
+    }
 }
 
 sub decode { return codec("decode", @_) }
