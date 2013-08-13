8735e479c60777a2d8dec4093c643bf64ed752b1
Ricardo SIGNES <rjbs@cpan.org>
2013-08-13 01:13:26 +0000

    Import of RJBS/Email-MIME-Encodings-1.315 from CPAN.
    gitpan-cpan-distribution: Email-MIME-Encodings
    gitpan-cpan-version:      1.315
    gitpan-cpan-path:         RJBS/Email-MIME-Encodings-1.315.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/MIME/Encodings.pm
+++ lib/Email/MIME/Encodings.pm
@@ -1,16 +1,18 @@
-package Email::MIME::Encodings;
 use strict;
-no strict 'refs';
 use warnings;
+package Email::MIME::Encodings;
+{
+  $Email::MIME::Encodings::VERSION = '1.315';
+}
+# ABSTRACT: A unified interface to MIME encoding and decoding
 
-$Email::MIME::Encodings::VERSION = "1.314";
-
-use MIME::Base64;
-use MIME::QuotedPrint;
+use MIME::Base64 3.05;
+use MIME::QuotedPrint 3.05;
 
 sub identity { $_[0] }
 
 for (qw(7bit 8bit binary)) {
+    no strict 'refs';
     *{"encode_$_"} = *{"decode_$_"} = \&identity;
 }
 
@@ -63,9 +65,17 @@ sub encode { return codec("encode", @_) }
 
 1;
 
+__END__
+
+=pod
+
 =head1 NAME
 
 Email::MIME::Encodings - A unified interface to MIME encoding and decoding
+
+=head1 VERSION
+
+version 1.315
 
 =head1 SYNOPSIS
 
@@ -83,27 +93,31 @@ This module simply wraps C<MIME::Base64> and C<MIME::QuotedPrint>
 so that you can throw the contents of a C<Content-Transfer-Encoding>
 header at some text and have the right thing happen.
 
-=head1 PERL EMAIL PROJECT
-
-This module is maintained by the Perl Email Project.
-
-L<http://emailproject.perl.org/wiki/Email::MIME::Encodings>
-
-=head1 AUTHOR
-
-Simon Cozens, C<simon@cpan.org>
-
-=head1 SEE ALSO
-
 C<MIME::Base64>, C<MIME::QuotedPrint>, C<Email::MIME>.
+
+=head1 AUTHORS
+
+=over 4
+
+=item *
+
+Simon Cozens <simon@cpan.org>
+
+=item *
+
+Casey West <casey@geeknest.com>
+
+=item *
+
+Ricardo SIGNES <rjbs@cpan.org>
+
+=back
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2004, Casey West F<<casey@geeknest.com>>.
+This software is copyright (c) 2004 by Simon Cozens and Casey West.
 
-Copyright 2003 by Simon Cozens
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself. 
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
